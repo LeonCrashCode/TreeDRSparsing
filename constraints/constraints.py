a0a4ea9209877666b782a53d79b998601d475654
Jiangming <jmliunlp@gmail.com>
2018-11-12 22:15:37 +0000

    fix master

--- constraints/constraints.py
+++ constraints/constraints.py
@@ -228,7 +228,7 @@ class relation_constraints:
 					self._assign(re, self.starts[RELATION], self.ends[RELATION], 1)
 					self._assign(re, self.starts[PREDICATE], self.ends[PREDICATE], 1)
 					self._assign(re, self.size, self.size+self.copy_length-1, 1)
-				self._assign(re, self.sep, self.sep, 1)
+			self._assign(re, self.sep, self.sep, 1)
 		elif self.cond == self.SDRS:
 			if self.d_rel == 0:
 				self._assign(re, self.starts[DISCOURSE], self.ends[DISCOURSE], 1)
