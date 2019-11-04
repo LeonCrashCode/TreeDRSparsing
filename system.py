91608989ba084db877d32ac7289107c6f7dc1c1d
jiangmingl <jiangmingl@jiangmingl0218.local>
2019-11-03 18:42:33 -0800

    clean for lossless

--- system.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def system_check_and_init(args):
-	if args.gpu:
-		assert torch.cuda.is_available(), "GPU is not available."
-	if args.gpu:
-		torch.cuda.manual_seed_all(args.random_seed)
-	torch.manual_seed(args.random_seed)
